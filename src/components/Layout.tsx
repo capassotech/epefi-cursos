6913d7d4e5dc38b0f759f8a26c5e8332533c229b
Karen Celes <karenceles13@gmail.com>
2026-05-06 22:22:32 -0300

    [FIX]EPEFI-79:Scroll no fluido en perfil

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,9 @@ import Header from "./Header";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-dvh min-h-screen bg-background text-foreground">
       <Header />
-      {/* overflow-anchor:none evita microajustes de scroll en Chrome cuando el árbol React se repinta */}
-      <main className="pb-20 md:pb-8 overflow-x-hidden [overflow-anchor:none]">
+      <main className="pb-20 md:pb-8 overflow-x-hidden">
         <Outlet />
       </main>
       <BottomNavigation />
